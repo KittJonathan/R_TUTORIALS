3dc4260639b0bce529893a4d85536c58d18c3ffb
Jonathan Kitt <jonathan.kitt@inrae.fr>
2024-11-26 14:21:28 +0100

    update

---
+++ 02-SCRIPTS/01_PREMIERS_PAS_AVEC_R.R
@@ -0,0 +1,61 @@
+# ATELIERS CODONS
+# PREMIERS PAS AVEC R
+
+# ❓ QU'EST-CE QUE R ? -----------------------------------------------------
+
+# R est un langage de programmation et un logiciel pour les calculs
+# statistiques et la création de graphiques.
+
+# Conçu en 1992 par Ross Ihaka et Robert Gentleman comme implémentation
+# open source du langage de programmation S, et publié en 1995.
+
+# Depuis, R s'est très largement développé et est utilisé pour :
+# - conduire des analyses statistiques et des workflows de data science
+# - créer des visualisations de haute complexité, prêtes à être publiées
+# - générer des rapports automatiques
+# - développer des applications web 
+# - créer des diapositives de présentation, des livres, et des pages web
+
+# Avantages de R :
+# - libre, open source, et plateforme indépendant
+# - large variété d'extensions proposant des foncionnalités supplémentaires
+# - hautement compatible avec de nombreux autres langages de programmation
+# - très puissant pour l'analyse et la visualisation de données
+# - souvent considéré comme "simple à coder" (d'une perspective d'un non programmeur)
+
+# Inconvénients de R :
+# - performance : "scalability", mémoire et vitesse
+# - courbe d'apprentissage (comme tout langage de programmation)
+# - problèmes potentiels de sécurité (pertinent pour les applications web)
+# - souvent considéré comme "étrange à coder" (d'une perspective d'un programmeur)
+
+# 🆚 R VS. RSTUDIO --------------------------------------------------------
+
+# Si R est le "moteur", RStudio est le tableau de bord.
+
+# RStudio est un IDE (Integrated Development Environment) open source pour R.
+
+# IDE le plus populaire depuis plusieurs années
+
+# Nombreuses fonctionnalités et extensions qui facilitent les workflows, comme
+# le suivi de version, les tables des matières, les add-ins, ...
+
+# Possibilité de travailler dans des projets R et d'utiliser RMarkdown / Quarto
+
+# ⬇️ INSTALLER R ET RSTUDIO -----------------------------------------------
+
+# Pour installer R : 
+# cloud.r-project.org
+
+# Pour installer RStudio :
+# posit.co/download/rstudio-desktop
+
+# Possibilité de télécharger R depuis le site web de Posit.
+
+# 🔎 RSTUDIO --------------------------------------------------------------
+
+# Une fenêtre "classique" de RStudio se divise en 4 panneaux :
+# - Le script
+# - La console (+ terminal et jobs)
+# - L'environnement (+ historique, build, suivi de version)
+# - Les plots (+ fichiers, packages, aide, viewer, presentations)
